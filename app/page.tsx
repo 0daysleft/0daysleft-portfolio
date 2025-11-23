b6f2ac909ff74ea883803feb37f0c9d1f84d0658
Joel Mikuro <joelmikuro@gmail.com>
2025-11-23 11:57:16 +0300

    added some text and function to the hero file and imported the components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
       <h1 className="">Hello Next JS</h1>
+      <div>
+        <Hero/>
+      </div>
     </main>
   );
 }

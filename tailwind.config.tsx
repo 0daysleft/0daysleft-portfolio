8a3037fa9a6558d92b85ddbb7365c341471511ad
Joel Mikuro <joelmikuro@gmail.com>
2025-11-23 12:13:01 +0300

    adding a default color in tailwind config file

--- tailwind.config.tsx
+++ tailwind.config.tsx
@@ -1,9 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        navyDark: "#000319",
+        black:{
+          DEFAULT: "#000",
+          100: "#000319",
+        }
+        
       },
     },
   },
+  plugins: [],
 };

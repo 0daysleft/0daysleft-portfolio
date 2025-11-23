783763633e710de7dd32bd18571e24de27209d6b
Joel Mikuro <joelmikuro@gmail.com>
2025-11-23 12:06:21 +0300

    styling the div section in main

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sn:px-10 px-5">
-      <h1 className="bg-navydark">Hello Next JS</h1>
-      <div>
+      <div className="max-w-7xl w-full">
         <Hero/>
       </div>
     </main>

12fd0066f0ac4d7da68b835e24381fec37629859
0daysleft <vincentarasa254@gmail.com>
2025-11-25 00:00:02 +0300

    adding some positioning to the header name

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
         <Spotlight className="top-40 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-[#000319] bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2]">
+      <div className="absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-white dark:bg-[#000319] bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2]">
         <div
           className={cn(
             "absolute inset-0",
@@ -29,6 +29,13 @@ const Hero = () => {
         />
         {/* Radial gradient for the container to give a faded look */}
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-[#000319]"></div>
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center" >
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue max-w-80">
+            Dynamic Web Magic With Next JS
+          </h2>
+        </div>
       </div>
     </div>
   );

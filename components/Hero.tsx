9d1423845a4168bfaba8be481628378ce884695d
0daysleft <vincentarasa254@gmail.com>
2025-11-24 23:04:54 +0300

    fix: comment out unused tw-animate-css import; add spotlight components in Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import { Spotlight } from "./ui/spotlight";
 import React from "react"
 
 const Hero = () => {
@@ -5,6 +6,9 @@ const Hero = () => {
     <div className="pb-20 pt-36" >
       <div>
         <h1 className="">Hero Stuff</h1>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
       </div>
     </div>
   )

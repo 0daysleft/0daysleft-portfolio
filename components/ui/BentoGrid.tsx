3b630e95ed327226f71ad4d492d71361850df1c8
0daysleft <vincentarasa254@gmail.com>
2025-11-25 17:46:33 +0300

    adding styling and gradient to the elemnts

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -10,8 +10,10 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto"
+        ,
         className
+
       )}
     >
       {children}
@@ -47,13 +49,13 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        background: 'rgb(4,7,29)',
-        backgroundColor: 'gradient...'
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
       
+      <div className="transition duration-200 group-hover/bento:translate-x-2">
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>

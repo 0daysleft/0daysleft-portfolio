45298fb520945cc2e822aa75552e257d4842fba2
0daysleft <vincentarasa254@gmail.com>
2025-11-25 18:14:46 +0300

    removed extra description

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -84,15 +84,16 @@ export const BentoGridItem = ({
 
         <div className={cn(
           titleClassName, 'group-hover/ bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
-        )}></div>
+        )}>
+          <div>
+            {description}
+          </div>
+        </div>
       </div>
 
       <div className="transition duration-200 group-hover/bento:translate-x-2">
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
         </div>
       </div>
     </div>

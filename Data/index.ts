e00d3fef41bfa88dc8482f30b9923aae95421045
0daysleft <vincentarasa254@gmail.com>
2025-11-25 16:47:20 +0300

    added data to the grid and intoduced a new folder with the grid elements

---
+++ Data/index.ts
@@ -0,0 +1,10 @@
+export const gridItems = [
+  {title: 'Title 1', description: 'Decs 1', id: 1},
+  {title: 'Title 1', description: 'Decs 1', id: 2},
+  {title: 'Title 1', description: 'Decs 1', id: 3},
+  {title: 'Title 1', description: 'Decs 1', id: 4},
+  {title: 'Title 1', description: 'Decs 1', id: 5},
+  {title: 'Title 1', description: 'Decs 1', id: 6},
+  {title: 'Title 1', description: 'Decs 1', id: 7},
+  {title: 'Title 1', description: 'Decs 1', id: 8}
+]
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,13 +1,13 @@
 import { section } from "motion/react-client";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/Data";
 
 export default function Grid(){
   return(
     <section id="about">
       <BentoGrid>
-            {
-              [{title: 'Title 1', description: 'Desc 1', id: 1}].map( (item) => (
+            { gridItems.map( (item) => (
                 <BentoGridItem 
                 id={item.id}
                 key={item.id}

41477e7673bd6293388fa507a2dd52526d0372d8
0daysleft <vincentarasa254@gmail.com>
2025-11-25 17:24:26 +0300

    adding variables types

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -23,16 +23,22 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
-  id
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-  id: number
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div

1b286ede511c270069a11867e964368e20296cf9
0daysleft <vincentarasa254@gmail.com>
2025-11-25 16:03:43 +0300

    imported a react home icon and fixed some typo errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/Hero";
-import { Spotlight } from "lucide-react";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { IoHome } from "react-icons/io5";
 
 export default function Home() {
   return (
     <main className="relative bg-[#000319] flex justify-center items-center flex-col overflow-hidden mx-auto sn:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Hero/>
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <IoHome /> }]}
+        />
+        <Hero />
       </div>
     </main>
   );

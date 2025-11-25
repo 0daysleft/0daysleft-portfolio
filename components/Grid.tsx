0da348442856d9ce8e9b734456c0d9c465e40279
0daysleft <vincentarasa254@gmail.com>
2025-11-25 16:35:10 +0300

    adding props and grid items to the layout

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,9 +1,23 @@
+import { section } from "motion/react-client";
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 export default function Grid(){
   return(
-    <>
-    </>
+    <section id="about">
+      <BentoGrid>
+            {
+              [{title: 'Title 1', description: 'Desc 1', id: '1'}].map( (item) => (
+                <BentoGridItem 
+                id={item.id}
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                />
+              ) )
+            }
+      </BentoGrid>
+    </section>
   )
 }
 

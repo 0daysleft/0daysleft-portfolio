7693d98c8bce85819297a9a3f1eb36ee35f25052
0daysleft <vincentarasa254@gmail.com>
2025-11-26 23:35:49 +0300

    instead of using a return keyword, I used brackets instead

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,11 +10,11 @@ export default function RecentProjects(){
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {
-          projects.map(({id,title,des,img,iconLists,link}) => {
-            return <div key={id} className="lg:min-h-[32.5rem] h=[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
+          projects.map(({id,title,des,img,iconLists,link}) => (
+            <div key={id} className="lg:min-h-[32.5rem] h=[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
               {title}
             </div>
-          })
+          ))
         }
       </div>
     </div>

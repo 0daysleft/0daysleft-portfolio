995a3122df3c7a40737c594ed88b6bea941fae19
0daysleft <vincentarasa254@gmail.com>
2025-11-26 12:46:35 +0300

    fixing sysntax errors

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -77,7 +77,7 @@ export const BentoGridItem = ({
           )}
         </div>
         <div
-          className={`absolute right-0 -bottom-5 &{id===5 && 'w-full opacity-80'}`}
+          className={`absolute right-0 -bottom-5 ${id===5 && 'w-full opacity-80'}`}
         >
           {spareImg && (
             <img

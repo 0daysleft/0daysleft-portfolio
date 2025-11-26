b1da358e7d68fb462f911c828ea6402b68390b49
0daysleft <vincentarasa254@gmail.com>
2025-11-27 01:40:01 +0300

    adding details to the nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,18 @@ import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { IoHome } from "react-icons/io5";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/Data";
 
 export default function Home() {
   return (
     <main className="relative bg-[#000319] flex justify-center items-center flex-col overflow-hidden mx-auto sn:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <IoHome /> }]}
+          navItems={navItems}
         />
         <Hero />
         <Grid />
-        <RecentProjects/>
+        <RecentProjects />
       </div>
     </main>
   );

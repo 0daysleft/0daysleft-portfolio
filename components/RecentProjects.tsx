2efebba350ed833168009773604da1a0b8743a18
0daysleft <vincentarasa254@gmail.com>
2025-11-26 23:56:50 +0300

    removed the code to add it to a separate component

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -16,19 +16,7 @@ export default function RecentProjects() {
             key={id}
             className="lg:min-h-[32.5rem] h=[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
-            <div className="h-[40rem] w-full flex items-center justify-center ">
-              <PinContainer title={title} href={link}>
-                <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                  <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                    {title}
-                  </h3>
-                  <div className="text-base !m-0 !p-0 font-normal">
-                    <span className="text-slate-500 ">{des}</span>
-                  </div>
-                  <img src={img} alt="kf" />
-                </div>
-              </PinContainer>
-            </div>
+           {title}
           </div>
         ))}
       </div>

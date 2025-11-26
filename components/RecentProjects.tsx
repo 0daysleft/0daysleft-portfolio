b1e5d0d7ca8f8eddbe0bcd6a7da5f9157f14fa29
0daysleft <vincentarasa254@gmail.com>
2025-11-27 01:14:35 +0300

    fixing syntax bugs and typo error

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -15,37 +15,45 @@ export default function RecentProjects() {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h=[25rem] flex items-center justify-center sm:w-96 w-[80vw] lg:h-[30vh] mb-10"
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] lg:h-[30vh] mb-10"
           >
-           <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vh] overflow-hidden h-[20vh]">
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vh] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
-                  <img src={img}
-                  alt={title}
-                  className="z-10 absolute bottom-1" />
+                <img src={img} alt={title} className="z-10 absolute bottom-1" />
               </div>
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">{title }</h1>
-              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-1">{des}</p>
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
 
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
-                  {
-                    iconLists.map( (icon,index) => (
-                      <div key={icon} className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center" style={
-                        {
-                          transform: `translateX(-${5 * index * 2}px)`
-                        }
-                      }>
-                        <img src={icon} alt={icon} className="p-2" />
-                      </div>
-                    ) )
-                  }
+                  {iconLists.map((icon, index) => (
+                    <div
+                      key={icon}
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt={icon} className="p-2" />
+                    </div>
+                  ))}
                 </div>
-                <div className="flex justify-center items-center"> <p className="flex lg:text-xl md:text-xs text-sm">Check Out Live Site</p> <FaLocationArrow className="ms-3" color="#Cbacf9"/> </div>
+                <div className="flex justify-center items-center">
+                  {" "}
+                  <p className="flex lg:text-xl md:text-xs text-sm">
+                    Check Out Live Site
+                  </p>{" "}
+                  <FaLocationArrow className="ms-3" color="#Cbacf9" />{" "}
+                </div>
               </div>
-           </PinContainer>
+            </PinContainer>
           </div>
         ))}
       </div>

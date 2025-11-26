1714cda4ff9c80cc821a0e2f7ac050eb9d6b9270
0daysleft <vincentarasa254@gmail.com>
2025-11-27 01:06:42 +0300

    fixing color sysntax related issues

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -9,7 +9,7 @@ export default function RecentProjects() {
     <div className="py-20">
       <h1 className="heading">
         A Small Selection of{" "}
-        <span className="text-purple">Recent Projects</span>
+        <span className="text-[purple]">Recent Projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (

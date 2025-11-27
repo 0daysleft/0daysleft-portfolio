d064c6f6c15659ff09e8a1eb9bc30701c1fc4c14
0daysleft <vincentarasa254@gmail.com>
2025-11-27 11:32:44 +0300

    setting up the layout and styling

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,5 +1,11 @@
-
-
-export default function Clients(){
-  return()
+export default function Clients() {
+  return (
+    <div className="py-20" id="clients">
+      <h1 className="heading">
+        Kind Words From{" "}
+        <span className="text-[purple]">Satisfied Clients</span>
+      </h1>
+      <div className="flex flex-col items-center"></div>
+    </div>
+  );
 }

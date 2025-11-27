26597ccf404b6f3b8b6a746180f4486be04d2977
0daysleft <vincentarasa254@gmail.com>
2025-11-27 12:35:05 +0300

    added an experience component and its default code

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { IoHome } from "react-icons/io5";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/Data";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );

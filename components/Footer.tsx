c8b1c81cd7798e38f21606a9f6cc014c1dcdd917
0daysleft <vincentarasa254@gmail.com>
2025-11-27 17:12:02 +0300

    adding styles to the footer social media icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -49,9 +49,9 @@ export default function Footer(){
           {
             socialMedia.map(
               (profile) => (
-                <div key={profile.id}>
+                <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blue-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
                   <img src={profile.img}
-                  alt={profile.id}
+                  alt="Image of a social media link"
                   width={20}
                   height={20}
                   />

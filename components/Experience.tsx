2c1fba0a6a66b7118d6889f8d8e72d5984387e42
0daysleft <vincentarasa254@gmail.com>
2025-11-27 12:41:46 +0300

    imported a work experience json data

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,6 +1,20 @@
-export default function Experience(){
-  return(
-    <>
-    </>
-  )
+import { workExperience } from "@/Data/index";
+
+export default function Experience() {
+  return (
+    <div className="py-20" id="experience">
+      <h1 className="heading">
+        My <span className="text-[purple]">Work Experience</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {
+          workExperience.map(
+            (card) => (
+
+            )
+          )
+        }
+      </div>
+    </div>
+  );
 }

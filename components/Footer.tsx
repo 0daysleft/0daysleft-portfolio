45dcfdf0efb9c44e8279203f137978d2bcfe25d2
0daysleft <vincentarasa254@gmail.com>
2025-11-27 16:47:07 +0300

    adding contents to the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,44 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
 
 export default function Footer(){
-  return(
-    <div className=""></div>
-  )
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="footer-grid"
+          className="w-full h-full opacity-50"
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready To Take <span className="text-[purple]">Your</span> Digital
+          Presence To The Next Level?
+        </h1>
+        <p className="text=white-200 md:mt-10 my-5 text-center">
+          Reach Out To Me Today And Let&apos;s Discuss How I Can Help You
+          Achieve Your Goals.
+        </p>
+
+        <a href="mailto:vincentarasa254@gmail.com">
+          <MagicButton
+            title="Let's get in Touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+
+      <div className="">
+        <p className="md:text-base text-sm">
+          Copyright © {new Date().getUTCFullYear()}{" "}
+          <a href="https://github.com/0daysleft" className="decoration-none">
+            Vincent Okemwa @<span className="text-[purple]">0daysleft</span>
+          </a>{" "}
+        </p>
+      </div>
+    </footer>
+  );
 }

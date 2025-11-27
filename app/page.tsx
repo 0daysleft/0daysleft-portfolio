12a95c57f292aefaeea019ae94046bd7cf9d159b
0daysleft <vincentarasa254@gmail.com>
2025-11-27 18:15:57 +0300

    fixing layout issues

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,16 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-[#000319] flex justify-center items-center flex-col mx-auto sn:px-10 px-5 overflow-clip max-w-[">
-      <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+    <main className="relative bg-[#000319] flex justify-center items-center flex-col mx-auto sn:px-10 px-5 overflow-clip">
+      <div className="max-w-[70vw] w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
-        <Footer/>
+        <Footer />
       </div>
     </main>
   );
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -15,23 +15,23 @@ export default function RecentProjects() {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[20.5rem] flex items-center justify-center sm:w-[370px] w-[80vw]"
           >
             <PinContainer
               title={link}
               href={link}
-              className="sm:w-[570px] w-[80vw]"
+              className="sm:w-[370px] w-[60vw]"
             >
               <div className="relative flex items-center justify-center overflow-hidden sm:h-[40vh] h-[30vh] mb-10 ">
-                <div className="relative w-full h-full overflow-hidden     lg:rounded-3xl bg-[#13162d]">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
                 <img src={img} alt={title} className="z-10 absolute bottom-1" />
               </div>
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+              <h1 className="font-bold lg:text-lg md:text-xl text-xs line-clamp-1">
                 {title}
               </h1>
-              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+              <p className="lg:font-normal font-light text-base line-clamp-2">
                 {des}
               </p>
 
@@ -40,18 +40,18 @@ export default function RecentProjects() {
                   {iconLists.map((icon, index) => (
                     <div
                       key={icon}
-                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      className="border border-white/[0.2] rounded-full bg-black w-8 h-8 flex justify-center items-center"
                       style={{
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt={icon} className="p-2" />
+                      <img src={icon} alt={icon} className="p-1" />
                     </div>
                   ))}
                 </div>
                 <div className="flex justify-center items-center">
                   {" "}
-                  <p className="flex lg:text-xl md:text-xs text-sm">
+                  <p className="flex md:text-xs text-base">
                     Check Out Live Site
                   </p>{" "}
                   <FaLocationArrow className="ms-3" color="#Cbacf9" />{" "}
